f3df8b496ca788dfc2f588bd965bb1407da44a98
WebCodeBox <79010745+WebCodeBox@users.noreply.github.com>
2021-04-15 16:32:18 +0200

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,66 @@
+### PREPARATION
+
+# load packages
+library(dplyr)
+
+# download the dataset
+filename <- "CourseProject_DataScience3.zip"
+
+# check if file already exists
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename, method = "curl")
+}
+
+# check if folder exists
+if (!file.exists("UCI HAR Dataset")) {
+  unzip(filename)
+}
+
+# assign data frames
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+
+##### STEP 1-5 #####
+
+### STEP 1: Merges the training and the test sets to create one data set.
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+Merged_Data <- cbind(Subject, X, Y)
+
+### STEP 2: Extracts only the measurements on the mean and standard deviation for each measurement.
+Tidy_Data <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+
+### STEP 3: Uses descriptive activity names to name the activities in the data set
+Tidy_Data$code <- activities[Tidy_Data$code, 2]
+
+### STEP 4: Appropriately labels the data set with descriptive variable names.
+names(Tidy_Data)[2] = "activity"
+names(Tidy_Data)<-gsub("Acc", "Accelerometer", names(Tidy_Data))
+names(Tidy_Data)<-gsub("Gyro", "Gyroscope", names(Tidy_Data))
+names(Tidy_Data)<-gsub("BodyBody", "Body", names(Tidy_Data))
+names(Tidy_Data)<-gsub("Mag", "Magnitude", names(Tidy_Data))
+names(Tidy_Data)<-gsub("^t", "Time", names(Tidy_Data))
+names(Tidy_Data)<-gsub("^f", "Frequency", names(Tidy_Data))
+names(Tidy_Data)<-gsub("tBody", "TimeBody", names(Tidy_Data))
+names(Tidy_Data)<-gsub("-mean()", "Mean", names(Tidy_Data), ignore.case = TRUE)
+names(Tidy_Data)<-gsub("-std()", "STD", names(Tidy_Data), ignore.case = TRUE)
+names(Tidy_Data)<-gsub("-freq()", "Frequency", names(Tidy_Data), ignore.case = TRUE)
+names(Tidy_Data)<-gsub("angle", "Angle", names(Tidy_Data))
+names(Tidy_Data)<-gsub("gravity", "Gravity", names(Tidy_Data))
+
+### STEP 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+AllAverageData <- Tidy_Data %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(AllAverageData, "AllAverageData.txt", row.name=FALSE)
+
+
